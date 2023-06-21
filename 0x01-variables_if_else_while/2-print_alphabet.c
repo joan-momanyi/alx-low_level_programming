48dac257b206e299fdf60a78a6aa606e09374d23
Joan-Momanyi <joannemomanyi7@gmail.com>
2023-06-20 23:25:00 -0700

    1-last_digit.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+*main - Entry point
+*Description: "get the last digit of a number"
+*Return: Always 0 (success)
+*/
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+		putchar("\n");
+		return (0);
+}
