ecca704a22ccb335408204fc576b57f688f3faa1
Joan-Momanyi <joannemomanyi7@gmail.com>
2023-06-22 21:27:37 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*Description: 'prints all possible combination of three'
+*Return: always 0
+*/
+int main(void)
+{
+	int a, b, c;
+
+	for (a = 0; a < 9; a++)
+	{
+	for (b = a + 1; a <= 9; b++)
+	{
+	for (c = b + 1; c <= 9; c++)
+	{
+	if ((b != a) != c)
+	{
+	putchar(a);
+	putchar(b);
+	putchar(c);
+	if (a == 7 && b == 8)
+	continue;
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
+}
+
