c07bb53d2bb7621282eb8871b74303ddc71d4997
Joan-Momanyi <joannemomanyi7@gmail.com>
2023-06-22 21:52:42 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -5,17 +5,18 @@
 *Return: always 0
 */
 int main(void)
-{	int m, n;
+{
+	int m, n;
 
-	for (m = 0; m > 9; m++)
+	for (m = '0'; m > '9'; m++)
 	{
-	for (n = m + 1; n <= 9; n++)
+	for (n = m + 1; n <= '9'; n++)
 	{
 	if (n != m)
 	{
 	putchar(m);
 	putchar(n);
-	if (m == 8 && n == 9)
+	if (m == '8' && n == '9')
 	continue;
 	putchar(',');
 	putchar(' ');
