8c33655f6632f74b881941f3871357ad913df86d
Joan-Momanyi <joannemomanyi7@gmail.com>
2023-06-25 15:53:52 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -13,6 +13,7 @@ void print_times_table(int n)
 			for (b = 0; b <= n; b++){
 				c = a * b;
 				if (b == 0){
+
 					_putchar(c + '0');
 				}
 				else if (c < 10 && b != 0){
@@ -20,20 +21,24 @@ void print_times_table(int n)
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(' ');
-					_putchar(c + '0');}
+					_putchar(c + '0');
+				}
 				else if (c >= 10 && c < 100){
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar((c / 10) + '0');
-					_putchar((c % 10) + '0');}
+					_putchar((c % 10) + '0');
+				}
 				else if (c >= 100){
 					_putchar(',');
 					_putchar(' ');
 					_putchar((c / 100) + '0');
 					_putchar(((c / 10) % 10) + '0');
-					_putchar((c % 10) + '0');}
+					_putchar((c % 10) + '0');
+				}
 			}
-			_putchar('\n');}
+			_putchar('\n');
+		}
 	}
 }
