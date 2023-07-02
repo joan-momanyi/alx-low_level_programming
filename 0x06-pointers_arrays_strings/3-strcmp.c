7d49377d3e676b2197a634179295603429d54d4e
Joan-Momanyi <joannemomanyi7@gmail.com>
2023-07-02 15:25:24 -0700

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
 *_strcmp - compare string values
-*s1: input value
-*s2: input value
+*@s1: input value
+*@s2: input value
 *Return: s1[i] - s2[i]
 */
 int _strcmp(char *s1, char *s2)
