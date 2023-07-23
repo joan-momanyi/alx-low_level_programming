468e641d7c092f7b1fe6c5db2fc29b8903db2f25
Joan-Momanyi <joannemomanyi7@gmail.com>
2023-07-23 00:16:25 -0700

    2-print_dog.c

---
+++ 0x0E-structures_typedef/2-main.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "dog.h"
+/**
+*main - check code
+*
+*Return: 0 always success
+*/
+int main(void)
+{
+	struct dog my_dog;
+
+	my_dog.name = "Poppy";
+	my_dog.age = 3.5;
+	my_dog.owner = "Bob";
+	print_dog(&my_dog);
+	return (0);
+}
